b38e4eafc2f6dec6a103d7a3e2901840b724e817
aldinnasrunm <adnzain40@gmail.com>
2023-08-30 09:54:57 +0700

    change the marquee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html","./dist/script/**/*.js"],
+  content: ["index.html", "./dist/script/**/*.js"],
   theme: {
     colors: {
       'primary': '#F35736',
       'dark': '#171513',
       'light': '#F3f3f3',
       'accent': '#e5e5e5',
+      'test': '#FF39C8',
     },
     // add Vidolaka font
     fontFamily: {
-      'sans': ['Vidolaka', 'sans-serif'],
+      'vidaloka': ['Vidolaka'],
+      'staatliches': ['Staatliches']
     },
-    extend: {},
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
